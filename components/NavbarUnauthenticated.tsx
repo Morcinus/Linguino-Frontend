44af7334daa31613e0c2309bd50a9e2e6f00e9dc
Morcinus <11.honzik@gmail.com>
2022-08-13 17:26:58 +0200

    feat: :sparkles: Add unauthenticated navbar

--- components/NavbarUnauthenticated.tsx
+++ components/NavbarUnauthenticated.tsx
@@ -1,21 +1,34 @@
 import * as React from "react";
 
+import { useTranslation } from "next-i18next";
+import Link from "next/link";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 
 export default function NavBarUnauthenticated() {
+  const { t } = useTranslation("common");
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
 
       <AppBar position="fixed" color="neutral">
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Unuthenticated
-          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Link href="/login">
+            <Button color="inherit" size="large">
+              {t("login")}
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button color="inherit" size="large">
+              {t("signup")}
+            </Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
