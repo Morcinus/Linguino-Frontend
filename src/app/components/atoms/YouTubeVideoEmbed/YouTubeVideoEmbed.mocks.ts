e686223a5c2fbed9e3772ff6a5a427a5bd8f2de6
Morcinus <11.honzik@gmail.com>
2023-02-27 14:02:01 -0600

    feat: :sparkles: Add YouTubeVideoEmbed component

---
+++ src/app/components/atoms/YouTubeVideoEmbed/YouTubeVideoEmbed.mocks.ts
@@ -0,0 +1,9 @@
+import { IYouTubeVideoEmbed } from "./YouTubeVideoEmbed";
+
+const base: IYouTubeVideoEmbed = {
+  videoId: "dQw4w9WgXcQ",
+};
+
+export const mockYouTubeVideoEmbedProps = {
+  base,
+};
