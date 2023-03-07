2d9918c5ac27e79d63701eb7c76efecd05e31a47
Morcinus <11.honzik@gmail.com>
2023-03-07 12:14:53 -0600

    refactor: :recycle: Refactor NavigationBars

---
+++ src/components/atoms/navigation/NavigationBars/NavigationBars.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import config from "config/config";
+import useAuth from "infrastructure/services/AuthProvider";
+import theme from "styles/theme";
+
+import { usePathname } from "next/navigation";
+
+import { Box, CssBaseline, Toolbar, useMediaQuery } from "@mui/material";
+
+import DrawerContainer from "../drawer/DrawerContainer/DrawerContainer";
+import BottomNavigationBar from "../main-navigation-bars/BottomNavigationBar/BottomNavigationBar";
+import SideNavigationBar from "../main-navigation-bars/SideNavigationBar/SideNavigationBar";
+import DefaultNavigationBar from "../top-navigation-bars/DefaultNavigationBar/DefaultNavigationBar";
+import UnauthenticatedNavigationBar from "../top-navigation-bars/UnauthenticatedNavigationBar/UnauthenticatedNavigationBar";
+
+export interface INavigation {}
+
+const Navigation: React.FC<INavigation> = () => {
+  const { user } = useAuth();
+
+  const pathname = usePathname();
+
+  const desktop = useMediaQuery(theme.breakpoints.up("md"));
+
+  return (
+    <>
+      {pathname && !config.pagesWithoutToolbar.includes(pathname) && (
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+
+          {user ? (
+            <DrawerContainer child={{ component: DefaultNavigationBar }} />
+          ) : (
+            <UnauthenticatedNavigationBar />
+          )}
+          {desktop === true ? <SideNavigationBar /> : <BottomNavigationBar />}
+
+          <Toolbar />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default Navigation;
