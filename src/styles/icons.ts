57844ea5bd1bc96cd3caf3d8b9a98306b829222b
Morcinus <11.honzik@gmail.com>
2023-03-07 12:19:52 -0600

    feat: :sparkles: Add icons file

---
+++ src/styles/icons.ts
@@ -0,0 +1,21 @@
+const icons: Record<string, string> = {
+  home: "home",
+  menu: "menu",
+
+  favorites: "favorite_border",
+  myVocabulary: "library_books_outlined",
+  courses: "apps_outlined",
+  help: "help_outline_outlined",
+  bugReport: "bug_report",
+  settings: "settings",
+  logout: "logout",
+
+  vocabulary: "extension",
+  grammar: "category",
+  pronunciation: "translate",
+  speaking: "record_voice_over",
+  reading: "auto_stories",
+  listening: "headphones",
+};
+
+export default icons;
