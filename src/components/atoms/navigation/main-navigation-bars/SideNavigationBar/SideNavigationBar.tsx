4adb779edbeedc8e29db6198c54a8c1c2dfe15f5
Morcinus <11.honzik@gmail.com>
2023-03-07 12:10:13 -0600

    refactor: :recycle: Refactor SideNavigationBar

--- src/components/atoms/navigation/main-navigation-bars/SideNavigationBar/SideNavigationBar.tsx
+++ src/components/atoms/navigation/main-navigation-bars/SideNavigationBar/SideNavigationBar.tsx
@@ -4,10 +4,12 @@ import { useRouter } from "next/navigation";
 
 import { Box, Tab, Tabs } from "@mui/material";
 
-import { useTranslation } from "../../../../i18n/client";
-import { primaryNavigation } from "../config";
+import { useTranslation } from "../../../../../i18n/client";
+import { primaryNavigation } from "../../config";
 
 export interface ISideNavigationBar {}
+
+export const SIDE_NAV_BAR_WIDTH = "90px";
 
 const SideNavigationBar: React.FC<ISideNavigationBar> = () => {
   const [value, setValue] = useState(0);
@@ -15,7 +17,17 @@ const SideNavigationBar: React.FC<ISideNavigationBar> = () => {
   const router = useRouter();
 
   return (
-    <Box sx={{ flexGrow: 1, display: "flex" }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        height: "100%",
+        width: SIDE_NAV_BAR_WIDTH,
+        position: "fixed",
+        left: 0,
+        top: 0,
+      }}
+    >
       <Tabs
         orientation="vertical"
         variant="scrollable"
@@ -26,6 +38,7 @@ const SideNavigationBar: React.FC<ISideNavigationBar> = () => {
         }}
         sx={{
           boxShadow: "2px 0 0 0 rgba(0,0,0,0.09)",
+          pt: "64px",
         }}
         TabIndicatorProps={{ style: { background: "rgba(0,0,0,0)" } }}
       >
