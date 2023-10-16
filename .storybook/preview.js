45bb7b193a5d4add9744842497d8a1f4a2d1458c
Morcinus <11.honzik@gmail.com>
2023-10-16 21:33:15 +0200

    feat: :sparkles: Change default Storybook settings

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -46,30 +46,6 @@ export const decorators = [
   },
 ];
 
-const BREAKPOINTS_INT = {
-  xs: 375,
-  sm: 600,
-  md: 900,
-  lg: 1200,
-  xl: 1536,
-};
-
-const customViewports = Object.fromEntries(
-  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
-    console.log(val);
-    return [
-      key,
-      {
-        name: key,
-        styles: {
-          width: `${val}px`,
-          height: `${(idx + 5) * 10}vh`,
-        },
-      },
-    ];
-  })
-);
-
 export const parameters = {
   nextRouter: {
     Provider: AppRouterContext.Provider,
@@ -84,5 +60,5 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  viewport: { viewports: customViewports },
+  layout: "centered",
 };
