f681c7c86e7bf84ef14c666bb2b1da7f4b995f51
Morcinus <11.honzik@gmail.com>
2024-02-05 15:13:01 +0100

    refactor: :recycle: Refactor Lessons page

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -1,49 +1,40 @@
 // prettier-ignore
 "use client"
 
-import { useTranslation } from "i18n/client";
 import { LessonType } from "infrastructure/api/user/courses/lessons/Lessons";
 import { isLessonType } from "infrastructure/api/user/courses/lessons/LessonsGuard";
 import useAuth from "infrastructure/services/AuthProvider";
-import icons from "styles/icons";
 
-import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
-import BottomFab from "components/atoms/BottomFab/BottomFab";
 import ContentContainer from "components/layouts/ContentContainer/ContentContainer";
 
 import LessonsOverview from "../../components/layouts/LessonsOverview/LessonsOverview";
 
-export interface ILessonsPage {
-  searchParams: {
-    type: LessonType;
-  };
-}
+export interface ILessonsPage {}
 
-const LessonsPage: React.FC<ILessonsPage> = ({ searchParams }) => {
-  const { t } = useTranslation("cs", "common");
-  const lessonType = searchParams.type;
-  const router = useRouter();
+const LessonsPage: React.FC<ILessonsPage> = () => {
+  const searchParams = useSearchParams();
   const { user } = useAuth();
+
+  function getLessonType(): LessonType | undefined {
+    const lessonType = searchParams?.get("type");
+    if (isLessonType(lessonType)) {
+      return lessonType;
+    } else return undefined;
+  }
+
+  const lessonType = getLessonType();
 
   return (
     <>
-      {user && (
-        <>
-          <ContentContainer>
-            <LessonsOverview
-              courseId={user.selectedCourse.id}
-              lessonType={searchParams.type}
-            />
-          </ContentContainer>
-          {isLessonType(lessonType) && (
-            <BottomFab
-              header={t("studying.study")}
-              icon={icons.next}
-              onClick={() => router.push(`/study?type=${lessonType}`)}
-            />
-          )}
-        </>
+      {user && lessonType && (
+        <ContentContainer>
+          <LessonsOverview
+            courseId={user.selectedCourse.id}
+            lessonType={lessonType}
+          />
+        </ContentContainer>
       )}
     </>
   );
