582ef9100b71939ded9c1dee6dd2e2e307da2852
Morcinus <11.honzik@gmail.com>
2024-02-10 11:28:24 +0100

    feat: :sparkles: Improve design

--- src/components/atoms/question-answers/matching-options/MatchTextOption/MatchTextOption.tsx
+++ src/components/atoms/question-answers/matching-options/MatchTextOption/MatchTextOption.tsx
@@ -31,9 +31,12 @@ const MatchTextOption: React.FC<IMatchTextOption> = ({
           borderStyle: selected === true ? "solid" : undefined,
           borderWidth: selected === true ? 2 : undefined,
           borderColor: selected === true ? "primary.main" : undefined,
+          py: 1.5,
         }}
+        fullWidth
         onClick={onClick}
         disabled={disabled}
+        size="large"
       >
         <Typography variant="h6" sx={{ textTransform: "none" }}>
           {text}
