ca4c7bd69704adc66904b2c60037915b8f88bcd0
Morcinus <11.honzik@gmail.com>
2024-02-11 14:57:47 +0100

    feat: :sparkles: Improve FreeTrialEndNotice design

--- src/components/atoms/notices/FreeTrialEndNotice/FreeTrialEndNotice.tsx
+++ src/components/atoms/notices/FreeTrialEndNotice/FreeTrialEndNotice.tsx
@@ -2,8 +2,10 @@ import { useTranslation } from "i18n/client";
 import { FreeTrialEndNotice as FreeTrialEndNoticeType } from "infrastructure/api/user/notices/Notices";
 import useNotices from "infrastructure/services/NoticeProvider";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+import { Box, Typography } from "@mui/material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 
@@ -22,9 +24,6 @@ const FreeTrialEndNotice: React.FC<IFreeTrialEndNotice> = ({ notice }) => {
 
   return (
     <FullscreenDialog
-      header1={t("notices.freeTrialEnded")}
-      header2={t("freeTrialEndNotice.title")}
-      imageURL="https://picsum.photos/id/168/512/512"
       primaryButton={{
         onClick: () => {
           deleteNotice(notice.id);
@@ -38,13 +37,39 @@ const FreeTrialEndNotice: React.FC<IFreeTrialEndNotice> = ({ notice }) => {
       }}
       transitionDuration={{ appear: 0, enter: 0, exit: 0 }}
     >
-      <List sx={{ listStyleType: "disc", pl: 4 }}>
-        {featureIdList.map((featureId, i) => (
-          <ListItem sx={{ display: "list-item" }} key={i}>
-            {t(`freeTrialEndNotice.${featureId}`)}
-          </ListItem>
-        ))}
-      </List>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        sx={{ width: "100%", gap: 4 }}
+      >
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          {t("notices.freeTrialEnded")}
+        </Typography>
+
+        <Box>
+          <Image
+            src={"/images/subscription/premium_1.svg"}
+            alt="Premium account"
+            width={400}
+            height={265}
+          />
+        </Box>
+
+        <Box>
+          <Typography variant="subtitle1" sx={{ textAlign: "center", mt: 2 }}>
+            {t("freeTrialEndNotice.title")}
+          </Typography>
+
+          <List sx={{ listStyleType: "disc", pl: 4 }}>
+            {featureIdList.map((featureId, i) => (
+              <ListItem sx={{ display: "list-item" }} key={i}>
+                {t(`freeTrialEndNotice.${featureId}`)}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Box>
     </FullscreenDialog>
   );
 };
