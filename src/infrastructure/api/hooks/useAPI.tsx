ea9e3d8b4951c78871597fe581e3dcaabe8693e1
Morcinus <11.honzik@gmail.com>
2024-02-15 11:08:37 +0100

    fix: :bug: Fix multiple error handler calls

--- src/infrastructure/api/hooks/useAPI.tsx
+++ src/infrastructure/api/hooks/useAPI.tsx
@@ -1,17 +1,24 @@
 import useSWR, { Key } from "swr";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import useErrorHandler from "../../services/ErrorHandler";
 import API, { SWRHook } from "../API";
 
 export default function useAPI<T>(key: Key): SWRHook<T> {
   const { data, error, mutate } = useSWR(key, API.get);
-
   const { setError } = useErrorHandler();
+  const errorHandledRef = useRef(false);
 
   useEffect(() => {
-    if (error) setError();
+    if (error && !errorHandledRef.current) {
+      setError();
+      errorHandledRef.current = true;
+    }
+
+    if (!error) {
+      errorHandledRef.current = false;
+    }
   }, [error, setError]);
 
   return {
