d13196959ecb6d288fb45c8bf6b1e9f0385cfb95
Morcinus <11.honzik@gmail.com>
2024-04-07 18:01:37 +0200

    refactor: :recycle: Remove placement test option

--- src/components/molecules/forms/SelectStartForm/config.ts
+++ src/components/molecules/forms/SelectStartForm/config.ts
@@ -7,10 +7,11 @@ export const startOptions: Array<StartOption> = [
     id: "selectLevel",
     imageUrl: "/images/account-setup/start_2.png",
   },
-  {
+  // Removed from production, backend endpoint isn't implemented yet
+  /* {
     id: "takeTest",
     imageUrl: "/images/account-setup/start_3.png",
-  },
+  }, */
 ];
 
 export interface StartOption {
